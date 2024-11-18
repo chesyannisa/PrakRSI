ca182bafcafda4999a14588738f3ddbd69305501
reishash <athaya27ramdhany@gmail.com>
2024-11-18 13:43:14 +0700

    refactor LoginController to integrate HalamanLoginUI and streamline user verification logic

--- src/main/java/com/controllers/LoginController.java
+++ src/main/java/com/controllers/LoginController.java
@@ -3,24 +3,25 @@
 // import src.main.java.com.models.RegisterLogin;
 // import src.main.java.com.providers.UserProvider;
 // import src.main.java.com.ui.HalamanUtamaUI;
+// import src.main.java.com.ui.HalamanLoginUI;
 
 // public class LoginController {
 //     private UserProvider userProvider;
 //     private HalamanUtamaUI halamanUtamaUI;
+//     private HalamanLoginUI halamanLoginUI;
     
-//     public LoginController() {
-//         userProvider = new UserProvider();
+//     public void TampilkanHalamanUtama() {
 //         halamanUtamaUI = new HalamanUtamaUI();
 //     }
-    
-//     public void TampilkanHalamanUtama(RegisterLogin login) {
-//         if (userProvider.isValidUser(login)) {
-//             halamanUtamaUI.tampilkan();
-//         }
-//     }
 
-//     public boolean verifikasiAkun(RegisterLogin login) {
-//         return userProvider.isValidUser(login);
+//     public boolean verifikasiAkun() {
+//         if (checkUsernamePassword(login)) {
+//             TampilkanHalamanUtama();
+//             return true;
+//         } else {
+//             halamanLoginUI.showErrorMessage("Username or password is incorrect.");
+//             return false;
+//         }
 //     }
 
 //     public boolean checkUsernamePassword(RegisterLogin login) {

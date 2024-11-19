4acb8b583f7db9c22d47232a59d97ac197771f6b
reishash <athaya27ramdhany@gmail.com>
2024-11-19 16:14:46 +0700

    Refactor HalamanRegisterUI to improve code formatting and readability

--- src/main/java/com/ui/HalamanRegisterUI.java
+++ src/main/java/com/ui/HalamanRegisterUI.java
@@ -36,7 +36,7 @@ public class HalamanRegisterUI extends JFrame {
         registerLogin = new RegisterLogin();
 
         // Gambar di bagian atas
-        ImageIcon originalIcon = new ImageIcon("src\\main\\resources\\logo.png"); 
+        ImageIcon originalIcon = new ImageIcon("src\\main\\resources\\logo.png");
         Image scaledImage = originalIcon.getImage().getScaledInstance(230, 150, Image.SCALE_SMOOTH);
         JLabel imageLabel = new JLabel(new ImageIcon(scaledImage), SwingConstants.CENTER);
         add(imageLabel, BorderLayout.NORTH);
@@ -118,8 +118,7 @@ public class HalamanRegisterUI extends JFrame {
         gbc.gridwidth = 2;
         gbc.anchor = GridBagConstraints.CENTER;
 
-
-        RoundedButton registerButton = new RoundedButton("Register",30);
+        RoundedButton registerButton = new RoundedButton("Register", 30);
         registerButton.setBackground(new Color(166, 179, 125)); // Warna tombol hijau
         registerButton.setForeground(Color.WHITE);
         registerButton.setFont(new Font("Nunito", Font.BOLD, 16)); // Font tombol
@@ -149,7 +148,7 @@ public class HalamanRegisterUI extends JFrame {
                 new HalamanLoginUI(null);
                 dispose();
             }
-            });
+        });
 
         setVisible(true);
     }
@@ -163,21 +162,18 @@ public class HalamanRegisterUI extends JFrame {
         String password = new String(passwordField.getPassword());
         String konfirmasiPassword = new String(konfirmasiPasswordField.getPassword());
         String roleUser = masyarakatButton.isSelected() ? "Masyarakat" : "Pengelola";
-        
 
         if (!password.equals(konfirmasiPassword)) {
             RegisterController registerController = new RegisterController();
             registerController.tampilkanPesanErrorKonfirmasi();
             return;
-        }
-        else {
+        } else {
             MenyimpanData(namaLengkap, tglLahir, namaIbu, username, password, roleUser);
             RegisterController registerController = new RegisterController();
             registerController.tampilkanPesanSuksesRegistrasi();
             LoginController loginController = new LoginController();
             loginController.tampilkanHalamanLogin();
         }
-
     }
 
     public void MenyimpanData(String namaLengkap, LocalDate tglLahir, String namaIbu, String username, String Password, String roleUser) {

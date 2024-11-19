d9c9563d95846b2d57a05b8580bc075f05a58c47
John Doe <johndoe@email.com>
2024-11-19 18:43:48 +0700

    connect

--- src/main/java/com/providers/DatabaseProvider.java
+++ src/main/java/com/providers/DatabaseProvider.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseProvider {
-    private static final String DB_URL = "http://127.0.0.1/phpmyadmin";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/resikinae_db";
     private static final String DB_USER = "root"; // Ganti dengan username database Anda
-    private static final String DB_PASSWORD = "Chesyaaa_23"; // Ganti dengan password database Anda
+    private static final String DB_PASSWORD = "BeLlA04_"; // Ganti dengan password database Anda
 
     public static Connection getConnection() throws SQLException {
         try {

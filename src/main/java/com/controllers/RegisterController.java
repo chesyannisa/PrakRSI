137203715dbfcec5fbe7d0de91fd7759069b6b0d
reishash <athaya27ramdhany@gmail.com>
2024-11-21 09:31:14 +0700

    balik

--- src/main/java/com/controllers/RegisterController.java
+++ src/main/java/com/controllers/RegisterController.java
@@ -6,30 +6,23 @@ import src.main.java.com.ui.HalamanRegisterUI;
 import src.main.java.com.providers.DatabaseProvider;
 
 public class RegisterController {
-    private DatabaseProvider databaseProvider;
-
-    public RegisterController(DatabaseProvider databaseProvider) {
-        this.databaseProvider = databaseProvider;
-    }
-
     // Method untuk menampilkan halaman register
     public void tampilkanHalamanRegister() {
         new HalamanRegisterUI();
     }
 
     // Method untuk memasukkan data user ke database
-    public void insertData(String namaLengkap, LocalDate tglLahir, String namaIbu, String username, String password, String roleUser) {
+    public void InsertData(String namaLengkap, LocalDate tglLahir, String namaIbu, String username, String password, String roleUser) {
+        DatabaseProvider databaseProvider = new DatabaseProvider();
         User newUser = new User(namaLengkap, tglLahir, namaIbu, username, password, roleUser);
         try {
             databaseProvider.addUser(newUser);
-            tampilkanPesanSuksesRegistrasi();
         } catch (SQLException e) {
             e.printStackTrace();
-            tampilkanPesanErrorKonfirmasi();
         }
     }
 
-    // Method untuk menampilkan pesan error konfirmasi
+    // Method untuk mengecek apakah username sudah ada di database
     public void tampilkanPesanErrorKonfirmasi() {
         HalamanRegisterUI.pesanErrorKonfirmasi();
     }
